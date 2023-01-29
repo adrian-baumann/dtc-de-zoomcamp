d8c99003c34e51f65cd2b12e6c8ca5681ea70287
Adrian <adrian.moritz@mail.de>
2023-01-29 21:31:03 +0100

    repeated homework with docker-compose and ingest_data.py

--- week1/README.md
+++ week1/README.md
@@ -5,7 +5,39 @@ Docker, SQL and Terraform
 ### Part A
 - Used [notebook](./upload-data.ipynb) from course and modified to upload data into PostgreSQL DB
 - Used pgcli to solve SQL questions about taxi data
+- later used [ingest_data.py](./ingest_data.py) and pgadmin in docker (see notes)
 - Queries used for Questions 3-6 found in [`homework-queries.sql`](./homework-queries.sql)
 ### Part B
 - tried terraform locally and afterwards setup in gcp virtual machine
 - posted output of `terraform apply` in homework google form
+
+------
+## Notes
+
+Run `ingest_data.py` with Docker
+1. To create docker containers in network run: 
+    ```bash
+    docker-compose up
+    ```
+
+2. Run
+    ```bash
+    docker build -t taxi_ingest:v001 ./week1/
+    ```
+2. Run with appropriate URL and table name
+    ```bash
+    URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+    NAME="yellow_taxi_data"
+
+    docker run -it \
+    --network=week1_default \
+    taxi_ingest:v001 \
+        --user=root \
+        --password=root \
+        --host=pgdatabase \
+        --port=5432 \
+        --db=ny_taxi \
+        --table_name=${NAME} \
+        --url=${URL}
+    ```
+
--- week1/ingest_data.py
+++ week1/ingest_data.py
@@ -30,20 +30,34 @@ def main(params):
 
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
 
-    df = pd.read_csv(csv_name)
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=10000)
 
-    for col in df.columns:
-        if "pickup_datetime" in col or "dropoff_datetime" in col:
-            df[col] = pd.to_datetime(df[col])
-            print(col, type(df[col][0]))
+    df = next(df_iter)
 
-    df.to_sql(
-        name=table_name,
-        con=engine,
-        chunksize=100000,
-        method="multi",
-        if_exists="replace",
-    )
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+
+    df.to_sql(name=table_name, con=engine, if_exists="append")
+
+    while True:
+
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+
+            for col in df.columns:
+                if "pickup_datetime" in col or "dropoff_datetime" in col:
+                    df[col] = pd.to_datetime(df[col])
+
+            df.to_sql(name=table_name, con=engine, if_exists="append")
+
+            t_end = time()
+
+            print("inserted another chunk, took %.3f second" % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 
 if __name__ == "__main__":
